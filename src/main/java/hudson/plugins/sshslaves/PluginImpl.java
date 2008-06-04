9259db504fbb01220804fb8bbc7c29e6641ad22c
stephenconnolly <stephenconnolly@dev.java.net>
2008-06-04 19:10:04 +0000

    adding start of ssh-slaves plugin

---
+++ src/main/java/hudson/plugins/sshslaves/PluginImpl.java
@@ -0,0 +1,21 @@
+package hudson.plugins.sshslaves;
+
+import hudson.Plugin;
+import hudson.slaves.ComputerLauncher;
+
+import java.util.logging.Logger;
+
+/**
+ * Entry point of ssh-slaves plugin.
+ *
+ * @author Stephen Connolly
+ * @plugin
+ */
+public class PluginImpl extends Plugin {
+
+    public void start() throws Exception {
+        //ComputerLauncher.LIST.add(SSHLauncher.DESCRIPTOR);
+    }
+
+    private static final java.util.logging.Logger LOGGER = Logger.getLogger(PluginImpl.class.getName());
+}
