91d59dc273efc368b643679ce994b9c2d62fb3c4
stephenconnolly <stephenconnolly@dev.java.net>
2008-06-08 10:00:01 +0000

    In theory this should work.... needs testing and tidy up

--- src/main/java/hudson/plugins/sshslaves/SSHLauncher.java
+++ src/main/java/hudson/plugins/sshslaves/SSHLauncher.java
@@ -2,8 +2,11 @@ package hudson.plugins.sshslaves;
 
 import com.trilead.ssh2.*;
 import hudson.model.Descriptor;
+import hudson.model.Messages;
+import hudson.remoting.Channel;
 import hudson.slaves.ComputerLauncher;
 import hudson.slaves.SlaveComputer;
+import hudson.util.StreamCopyThread;
 import hudson.util.StreamTaskListener;
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -11,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 /**
  * A computer launcher that tries to start a linux slave by opening an SSH connection and trying to find java.
@@ -37,130 +41,190 @@ public class SSHLauncher extends ComputerLauncher {
         return true;
     }
 
-    public synchronized void launch(SlaveComputer slaveComputer, StreamTaskListener listener) {
+    /**
+     * Gets the formatted current time stamp.
+     */
+    private static String getTimestamp() {
+        return String.format("[%1$tD %1$tT]", new Date());
+    }
+
+    public synchronized void launch(final SlaveComputer computer, final StreamTaskListener listener) {
         connection = new Connection(host, port);
-
         try {
-            // TODO open ssh connection to the host
-            listener.getLogger().println("[SSH] Opening SSH connection to " + host + ":" + port);
-            connection.connect();
+            openConnection(listener);
 
-            // TODO if using a key file, use the key file instead of password
-            listener.getLogger().println("[SSH] Authenticating as " + username + "/******");
-            boolean isAuthenicated = connection.authenticateWithPassword(username, password);
+            String java = findJava(listener);
 
-            if (isAuthenicated && connection.isAuthenticationComplete()) {
-                listener.getLogger().println("[SSH] Authentication successful.");
-            } else {
-                listener.getLogger().println("[SSH] Authentication failed.");
-                connection.close();
-                connection = null;
-                listener.getLogger().println("[SSH] Connection closed.");
-                return;
+            String workingDirectory = computer.getNode().getRemoteFS();
+            while (workingDirectory.endsWith("/")) {
+                workingDirectory = workingDirectory.substring(0, workingDirectory.length() - 1);
             }
 
-            listener.getLogger().println("[SSH] Checking default java version");
-            String line;
-            Session session = connection.openSession();
-            try {
-                session.execCommand("java -version");
-                StreamGobbler out = new StreamGobbler(session.getStdout());
-                StreamGobbler err = new StreamGobbler(session.getStderr());
-                try {
-                    BufferedReader r = new BufferedReader(new InputStreamReader(out));
+            copySlaveJar(listener, workingDirectory);
 
-                    // TODO make sure this works with IBM JVM & JRocket
+            startSlave(computer, listener, java, workingDirectory);
 
-                    while (null != (line = r.readLine()) && !line.startsWith("java version \"")) {
-                        listener.getLogger().println("  " + line);
-                    }
-                } finally {
-                    out.close();
-                    err.close();
-                }
-            } finally {
-                session.close();
-            }
-
-            if (line == null || !line.startsWith("java version \"")) {
-                throw new IOException("The default version of java is either unsupported version or unknown");
-            }
-
-            line = line.substring(line.indexOf('\"') + 1, line.lastIndexOf('\"'));
-            listener.getLogger().println("[SSH] java version = " + line);
-
-            // TODO check if the default java is 1.5 or newer
-
-            // TODO if not, find a java that is or throw an error
-
-            String fileDir = slaveComputer.getNode().getRemoteFS();
-            while (fileDir.endsWith("/")) {
-                fileDir = fileDir.substring(0, fileDir.length() - 1);
-            }
-            String fileName = fileDir + "/slave.jar";
-
-            listener.getLogger().println("[SSH] Starting sftp client...");
-            SFTPv3Client sftpClient = null;
-            try {
-                sftpClient = new SFTPv3Client(connection);
-
-                SFTPv3FileHandle fh = null;
-                try {
-                    // TODO decide best permissions and handle errors if exists already
-                    sftpClient.mkdir(fileDir, 0700);
-
-                    // TODO handle the file existing already
-                    listener.getLogger().println("[SSH] Copying latest slave.jar...");
-                    fh = sftpClient.createFile(fileName);
-
-                    InputStream is = null;
-                    try {
-                        // TODO get the slave jar the correct way... this may not be working
-                        is = getClass().getResourceAsStream("/WEB-INF/slave.jar");
-                        byte[] buf = new byte[2048];
-
-                        listener.getLogger().println("[SSH] Sending data...");
-
-                        int count = 0;
-                        int bufsiz = 0;
-                        try {
-                            while ((bufsiz = is.read(buf)) != -1) {
-                                sftpClient.write(fh, (long) count, buf, 0, bufsiz);
-                                count += bufsiz;
-                            }
-                            listener.getLogger().println("[SSH] Sent " + count + " bytes.");
-                        } catch (Exception e) {
-                            listener.getLogger().println("[SSH] Error writing to remote file");
-                            e.printStackTrace(listener.getLogger());
-                        }
-                    } finally {
-                        if (is != null) {
-                            is.close();
-                        }
-                    }
-                } catch (Exception e) {
-                    listener.getLogger().println("[SSH] Error creating file");
-                    e.printStackTrace(listener.getLogger());
-                }
-            } finally {
-                if (sftpClient != null) {
-                    sftpClient.close();
-                }
-            }
-
-            // TODO launch the slave.jar with a command that removes it once it terminates
-
-            // TODO set the channel to the STD I/O of the ssh connection
-
-            throw new IOException("Implementation not yet finished");
         } catch (IOException e) {
             e.printStackTrace(listener.getLogger());
             connection.close();
             connection = null;
             listener.getLogger().println("[SSH] Connection closed.");
         }
+    }
 
+    private void startSlave(SlaveComputer computer, final StreamTaskListener listener, String java, String workingDirectory) throws IOException {
+        final Session session = connection.openSession();
+        try {
+            // TODO handle escaping fancy characters in paths
+            session.execCommand("cd " + workingDirectory + " && " + java + " -jar slave.jar");
+            final StreamGobbler out = new StreamGobbler(session.getStdout());
+            final StreamGobbler err = new StreamGobbler(session.getStderr());
 
+            // capture error information from stderr. this will terminate itself
+            // when the process is killed.
+            new StreamCopyThread("stderr copier for remote agent on " + computer.getDisplayName(),
+                    err, listener.getLogger()).start();
+
+            computer.setChannel(out, session.getStdin(), listener.getLogger(), new Channel.Listener() {
+                public void onClosed(Channel channel, IOException cause) {
+                    if (cause != null) {
+                        cause.printStackTrace(listener.error(Messages.Slave_Terminated(getTimestamp())));
+                    }
+                    try {
+                        session.close();
+                    } catch (Throwable t) {
+                        t.printStackTrace(listener.error("closed"));
+                    }
+                    try {
+                        out.close();
+                    } catch (Throwable t) {
+                        t.printStackTrace(listener.error("closed"));
+                    }
+                    try {
+                        err.close();
+                    } catch (Throwable t) {
+                        t.printStackTrace(listener.error("closed"));
+                    }
+                }
+            });
+
+        } catch (InterruptedException e) {
+            session.close();
+            e.printStackTrace(listener.error("aborted"));
+        }
+    }
+
+    private void copySlaveJar(StreamTaskListener listener, String workingDirectory) throws IOException {
+        String fileName = workingDirectory + "/slave.jar";
+
+        listener.getLogger().println("[SSH] Starting sftp client...");
+        SFTPv3Client sftpClient = null;
+        try {
+            sftpClient = new SFTPv3Client(connection);
+
+            SFTPv3FileHandle fh = null;
+            try {
+                // TODO decide best permissions and handle errors if exists already
+                sftpClient.mkdir(workingDirectory, 0700);
+
+                // TODO handle the file existing already
+                listener.getLogger().println("[SSH] Copying latest slave.jar...");
+                fh = sftpClient.createFile(fileName);
+
+                InputStream is = null;
+                try {
+                    // TODO get the slave jar the correct way... this may not be working
+                    is = getClass().getResourceAsStream("/WEB-INF/slave.jar");
+                    byte[] buf = new byte[2048];
+
+                    listener.getLogger().println("[SSH] Sending data...");
+
+                    int count = 0;
+                    int bufsiz = 0;
+                    try {
+                        while ((bufsiz = is.read(buf)) != -1) {
+                            sftpClient.write(fh, (long) count, buf, 0, bufsiz);
+                            count += bufsiz;
+                        }
+                        listener.getLogger().println("[SSH] Sent " + count + " bytes.");
+                    } catch (Exception e) {
+                        listener.getLogger().println("[SSH] Error writing to remote file");
+                        e.printStackTrace(listener.getLogger());
+                    }
+                } finally {
+                    if (is != null) {
+                        is.close();
+                    }
+                }
+            } catch (Exception e) {
+                listener.getLogger().println("[SSH] Error creating file");
+                e.printStackTrace(listener.getLogger());
+            }
+        } finally {
+            if (sftpClient != null) {
+                sftpClient.close();
+            }
+        }
+    }
+
+    private String findJava(StreamTaskListener listener) throws IOException {
+        String java;
+        java = "java";
+        listener.getLogger().println("[SSH] Checking default java version");
+        String line;
+        Session session = connection.openSession();
+        try {
+            session.execCommand(java + " -version");
+            StreamGobbler out = new StreamGobbler(session.getStdout());
+            StreamGobbler err = new StreamGobbler(session.getStderr());
+            try {
+                BufferedReader r = new BufferedReader(new InputStreamReader(out));
+
+                // TODO make sure this works with IBM JVM & JRocket
+
+                while (null != (line = r.readLine()) && !line.startsWith("java version \"")) {
+                    listener.getLogger().println("  " + line);
+                }
+            } finally {
+                out.close();
+                err.close();
+            }
+        } finally {
+            session.close();
+        }
+
+        if (line == null || !line.startsWith("java version \"")) {
+            throw new IOException("The default version of java is either unsupported version or unknown");
+        }
+
+        line = line.substring(line.indexOf('\"') + 1, line.lastIndexOf('\"'));
+        listener.getLogger().println("[SSH] java version = " + line);
+
+        // TODO make this version check a bit less hacky
+        if (line.compareTo("1.5") < 0) {
+            // TODO find a java that is at least 1.5
+            throw new IOException("Could not find a version of java that is at least version 1.5");
+        }
+        return java;
+    }
+
+    private void openConnection(StreamTaskListener listener) throws IOException {
+        listener.getLogger().println("[SSH] Opening SSH connection to " + host + ":" + port);
+        connection.connect();
+
+        // TODO if using a key file, use the key file instead of password
+        listener.getLogger().println("[SSH] Authenticating as " + username + "/******");
+        boolean isAuthenicated = connection.authenticateWithPassword(username, password);
+
+        if (isAuthenicated && connection.isAuthenticationComplete()) {
+            listener.getLogger().println("[SSH] Authentication successful.");
+        } else {
+            listener.getLogger().println("[SSH] Authentication failed.");
+            connection.close();
+            connection = null;
+            listener.getLogger().println("[SSH] Connection closed.");
+            throw new IOException("Authentication failed.");
+        }
     }
 
     public synchronized void afterDisconnect(SlaveComputer slaveComputer, StreamTaskListener listener) {
