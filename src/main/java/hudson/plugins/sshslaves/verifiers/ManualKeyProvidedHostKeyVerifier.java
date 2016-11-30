6de6a334c751562b93c085a2220999c4c553fb19
Michael Clarke <michael.m.clarke@gmail.com>
2016-11-30 20:21:39 +0000

    Add facility to manually provide the host key to match against

---
+++ src/main/java/hudson/plugins/sshslaves/verifiers/ManualKeyProvidedHostKeyVerifier.java
@@ -0,0 +1,98 @@
+package hudson.plugins.sshslaves.verifiers;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import com.trilead.ssh2.signature.DSASHA1Verify;
+import com.trilead.ssh2.signature.RSASHA1Verify;
+
+import hudson.Extension;
+import hudson.model.TaskListener;
+import hudson.plugins.sshslaves.Messages;
+import hudson.plugins.sshslaves.SSHLauncher;
+import hudson.plugins.sshslaves.verifiers.HostKeyManager.HostKey;
+import hudson.remoting.Base64;
+import hudson.slaves.SlaveComputer;
+import hudson.util.FormValidation;
+
+public class ManualKeyProvidedHostKeyVerifier extends HostKeyVerifier {
+
+	private final HostKey key;
+	
+	@DataBoundConstructor
+	public ManualKeyProvidedHostKeyVerifier(String key) {
+		super();
+		this.key = parseKey(key);
+	}
+	
+	public String getKey() {
+		return key.getAlgorithm() + " " + Base64.encode(key.getKey());
+	}
+	
+	public HostKey getParsedKey() {
+		return key;
+	}
+	
+	@Override
+	public boolean verify(SlaveComputer computer, HostKey hostKey, TaskListener listener) throws Exception {
+		if (key.equals(hostKey)) {
+			listener.getLogger().println(Messages.ManualKeyProvidedHostKeyVerifier_KeyTrusted(SSHLauncher.getTimestamp()));
+			return true;
+		} else {
+			listener.getLogger().println(Messages.ManualKeyProvidedHostKeyVerifier_KeyNotTrusted(SSHLauncher.getTimestamp()));
+			return false;
+		}
+	}
+	
+	private static HostKey parseKey(String key) {
+		if (!key.contains(" ")) {
+			throw new IllegalArgumentException(Messages.ManualKeyProvidedHostKeyVerifier_TwoPartKey());
+		}
+		StringTokenizer tokenizer = new StringTokenizer(key, " ");
+		String algorithm = tokenizer.nextToken();
+		byte[] keyValue = Base64.decode(tokenizer.nextToken());
+		if (null == keyValue) {
+		throw new IllegalArgumentException(Messages.ManualKeyProvidedHostKeyVerifier_Base64EncodedKeyValueRequired());
+		}
+		
+		try {
+			if ("ssh-rsa".equals(algorithm)) {
+				RSASHA1Verify.decodeSSHRSAPublicKey(keyValue);
+			} else if ("ssh-dss".equals(algorithm)) {
+				DSASHA1Verify.decodeSSHDSAPublicKey(keyValue);
+			} else {
+				throw new IllegalArgumentException("Key algorithm should be one of ssh-rsa or ssh-dss");
+			}
+		} catch (IOException ex) {
+			throw new IllegalArgumentException(Messages.ManualKeyProvidedHostKeyVerifier_KeyValueDoesNotParse(algorithm), ex);
+		}  catch (StringIndexOutOfBoundsException ex) {
+			// can happen in DSASHA1Verifier with certain values (from quick testing)
+			throw new IllegalArgumentException(Messages.ManualKeyProvidedHostKeyVerifier_KeyValueDoesNotParse(algorithm), ex);
+		}
+		
+		return new HostKey(algorithm, keyValue);
+	}
+	
+	@Extension
+	public static class ManualKeyProvidedHostKeyVerifierDescriptor extends HostKeyVerifierDescriptor {
+
+		@Override
+		public String getDisplayName() {
+			return Messages.ManualKeyProvidedHostKeyVerifier_DisplayName();
+		}
+		
+		public FormValidation doCheckKey(@QueryParameter String key) {
+			try {
+				ManualKeyProvidedHostKeyVerifier.parseKey(key);
+				return FormValidation.ok();
+			} catch (IllegalArgumentException ex) {
+				return FormValidation.error(ex.getMessage());
+			}
+		}
+		
+	}
+
+}
