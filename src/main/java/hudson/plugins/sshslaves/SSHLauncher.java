bf5488b09029b4cf153dbc0b46a89094f249872f
Jannis Oeltjen <oss@jcoeltjen.de>
2018-08-14 00:34:50 +0200

    JENKINS-37152 Replace 'true' with 'echo' (#97)
    * JENKINS-37152 use 'exit 0' instead of echo redirection

--- src/main/java/hudson/plugins/sshslaves/SSHLauncher.java
+++ src/main/java/hudson/plugins/sshslaves/SSHLauncher.java
@@ -973,7 +973,7 @@ public class SSHLauncher extends ComputerLauncher {
      */
     private void verifyNoHeaderJunk(TaskListener listener) throws IOException, InterruptedException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        connection.exec("true",baos);
+        connection.exec("exit 0",baos);
         final String s;
         //TODO: Seems we need to retrieve the encoding from the connection destination
         try {
