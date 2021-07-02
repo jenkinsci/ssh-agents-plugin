1efd677326b8dffc629c8b245b35fef1718fe7f1
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2021-07-02 13:58:34 +0200

    [JENKINS-65929] Detection of docker is unreliable (#236)

--- src/test/java/hudson/plugins/sshslaves/rules/CheckIsDockerAvailable.java
+++ src/test/java/hudson/plugins/sshslaves/rules/CheckIsDockerAvailable.java
@@ -5,6 +5,7 @@ import org.junit.rules.ExternalResource;
 import org.testcontainers.DockerClientFactory;
 
 import java.io.IOException;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * Rule to check if Docker is available.
@@ -14,23 +15,6 @@ import java.io.IOException;
 public class CheckIsDockerAvailable extends ExternalResource {
   @Override
   protected void before() {
-    org.junit.Assume.assumeTrue(isDockerAvailable());
-  }
-
-  boolean isDockerAvailable() {
-    int exitCode = 0;
-    try {
-      ProcessBuilder builder = new ProcessBuilder();
-      if (SystemUtils.IS_OS_WINDOWS) {
-        builder.command("cmd.exe", "/c", "docker --version");
-      } else {
-        builder.command("sh", "-c", "docker --version");
-      }
-      Process process = builder.start();
-      exitCode = process.waitFor();
-    } catch (InterruptedException|IOException e) {
-      exitCode = 0;
-    }
-    return exitCode == 0;
+    assumeTrue(DockerClientFactory.instance().isDockerAvailable());
   }
 }
